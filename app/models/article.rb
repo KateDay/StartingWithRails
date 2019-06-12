bce500e27026919b6fe58d2b1cd886b6ffd7056b
KateDay <khummle@gmail.com>
2019-06-12 17:31:38 -0600

    ArticleNew: set up validator and display msg to user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
-    validates_presence_of :title, length: { minimum: 5 }
+    validates :title, presence: true,
+                        length: { minimum: 5 }
 end
