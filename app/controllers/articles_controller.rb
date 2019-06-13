e6fcf7c53e89495341fee3d0d7d24256396dbb9f
KateDay <khummle@gmail.com>
2019-06-12 18:04:26 -0600

    article: edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,9 +11,18 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @article = Article.new(article_params)
-
     if @article.save
       redirect_to @article
     else
